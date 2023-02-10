5fc5b6e2c1d488305dfe0821a6176dd3a9ed9cd2
darth-wyvern <nguyenquocnhan777@gmail.com>
2023-02-10 13:47:37 +0700

    Add architecture component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ArchitectureComponent from './component/ArchitectureComponent';
 
 function App() {
   return (
     <div className="App">
-      pokemon
+      <ArchitectureComponent />
     </div>
   );
 }
