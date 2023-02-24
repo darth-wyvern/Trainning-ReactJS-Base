6531fdd3d272e7954c9f86040879126221d4e1d9
darth-wyvern <nguyenquocnhan777@gmail.com>
2023-02-24 14:28:27 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import './App.css';
+import './styles/App.css';
 import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-const Dashboard = lazy(() => import('./page/Dashboard'));
 const Layout = lazy(() => import('./page/Layout'));
+const Dashboard = lazy(() => import('./page/Dashboard'));
+const Style = lazy(() => import('./page/Style'));
+const State = lazy(() => import('./page/State'));
 const Content = lazy(() => import('./page/Content'));
 const Architecture = lazy(() => import('./page/Architecture'));
-const Rendering = lazy(() => import('./page/Rendering'));
+const Props = lazy(() => import('./page/Props'));
+const Hook = lazy(() => import('./page/Hook'));
+const Redux = lazy(() => import('./page/Redux'));
+const ServerRendering = lazy(() => import('./page/ServerRendering'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
-        <Route path="rendering" element={<Rendering />} />
+        <Route path="state" element={<State />} />
+        <Route path="hook" element={<Hook />} />
+        <Route path="redux" element={<Redux />} />
+        <Route path="style" element={<Style />} />
+        <Route path="props" element={<Props />} />
         <Route path="content" element={<Content />} />
+        <Route path="server-rendering" element={<ServerRendering />} />
         <Route path="component-achitecture" element={<Architecture />} />
       </Route>
     </Routes>
